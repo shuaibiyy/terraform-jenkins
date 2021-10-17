d7a6d092aee2232480e143fc3215bf97459a263f
karannnn-exe <67971144+karannnn-exe@users.noreply.github.com>
2021-10-17 11:22:37 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,9 +1,3 @@
-provider "aws" {
-  access_key = "${var.access_key}"
-  secret_key = "${var.secret_key}"
-  region = "${var.region}"
-}
-
 data "terraform_remote_state" "tfstate" {
   backend = "s3"
 
