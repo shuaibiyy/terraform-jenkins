e684787b2944833277fd4fb7011f8a7f4870f167
alesiobitaGit <alesio100.ab@gmail.com>
2022-06-04 12:12:14 +0200

    change org name

--- provider.tf
+++ provider.tf
@@ -1,9 +1,22 @@
 terraform {
   required_version = ">=1.0.0"
+   aws = {
+      source = "hashicorp/aws"
+    }
+    random = {
+      source = "hashicorp/random"
+    }
 }
-
+/*
 provider "aws" {
   region  = var.aws_region
   profile = var.aws_profile
 }
+*/
+cloud {
+    organization = "My_ProjectJenkings"
 
+    workspaces {
+      name = "gh-actions-jenkings"
+    }
+}
